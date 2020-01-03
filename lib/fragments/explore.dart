bc91a9767dc54122ecdfb390cdcb198229ba13c2
leo <leonardo.emili3@gmail.com>
2020-01-03 12:22:15 +0100

    Working pull to refresh feature inside explore page

--- lib/fragments/explore.dart
+++ lib/fragments/explore.dart
@@ -11,40 +11,53 @@ class ExploreFragment extends StatefulWidget {
 }
 
 class _ExploreFragmentState extends State<ExploreFragment> {
+  final GlobalKey<RefreshIndicatorState> _refreshKey =
+      GlobalKey<RefreshIndicatorState>();
 
   @override
   Widget build(BuildContext context) {
     //setStatusBarDarkColor();
     return AnnotatedRegion<SystemUiOverlayStyle>(
-    value: SystemUiOverlayStyle.dark.copyWith(
-   statusBarColor: Colors.black, // Color for Android
-   statusBarBrightness: Brightness.light // Dark == white status bar -- for IOS.
-),
-    child: SafeArea(
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-          Padding(
-              padding: EdgeInsets.only(left: 20, top: Platform.isIOS ? 20 : 0),
-              child: Platform.isAndroid
-                  ? Text("")
-                  : Text("Esplora",
-                      style: TextStyle(
-                          fontSize: 40, fontWeight: FontWeight.bold))),
-          SizedBox(height: Platform.isAndroid ? 0 : 20),
-          Expanded(
-              child: ListView.builder(
-            itemCount: itineraries.length,
-            itemBuilder: (_, index) => Container(
-                // Add padding to the last item of the list
-                padding: index == itineraries.length - 1
-                    ? EdgeInsets.only(bottom: 10)
-                    : EdgeInsets.only(bottom: 0),
-                // Form a new card from the current itinerary information
-                child: ExploreCard(
-                  itinerary: itineraries[index],
-                )),
-          )),
-        ])));
+        value: SystemUiOverlayStyle.dark.copyWith(
+            statusBarColor: Colors.black, // Color for Android
+            statusBarBrightness:
+                Brightness.light // Dark == white status bar -- for IOS.
+            ),
+        child: SafeArea(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+              Padding(
+                  padding:
+                      EdgeInsets.only(left: 20, top: Platform.isIOS ? 20 : 0),
+                  child: Platform.isAndroid
+                      ? Text("")
+                      : Text("Esplora",
+                          style: TextStyle(
+                              fontSize: 40, fontWeight: FontWeight.bold))),
+              SizedBox(height: Platform.isAndroid ? 0 : 20),
+              Expanded(
+                  child: RefreshIndicator(
+                      key: _refreshKey,
+                      onRefresh: () => refreshList(),
+                      child: ListView.builder(
+                        itemCount: itineraries.length,
+                        itemBuilder: (_, index) => Container(
+                            // Add padding to the last item of the list
+                            padding: index == itineraries.length - 1
+                                ? EdgeInsets.only(bottom: 10)
+                                : EdgeInsets.only(bottom: 0),
+                            // Form a new card from the current itinerary information
+                            child: ExploreCard(
+                              itinerary: itineraries[index],
+                            )),
+                      ))),
+            ])));
+  }
+
+  Future<Null> refreshList() async {
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {});
+    return null;
   }
 }
--- lib/fragments/explore_visited.dart
+++ lib/fragments/explore_visited.dart
@@ -12,60 +12,62 @@ class ExploreVisitedFragment extends StatefulWidget {
 }
 
 class _ExploreVisitedFragmentState extends State<ExploreVisitedFragment> {
-  
   @override
   Widget build(BuildContext context) {
     //setStatusBarDarkColor();
     if (userVisits.isEmpty)
       return AnnotatedRegion<SystemUiOverlayStyle>(
-    value: SystemUiOverlayStyle.dark.copyWith(
-   statusBarColor: Colors.black, // Color for Android
-   statusBarBrightness: Brightness.light // Dark == white status bar -- for IOS.
-), child:
-      Center(
-          child: Text(
-        "Sembra che tu non abbia ancora seguito nessun itinerario.\nChe aspetti!!",
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          fontSize: 18,
-        ),
-      )));
+          value: SystemUiOverlayStyle.dark.copyWith(
+              statusBarColor: Colors.black, // Color for Android
+              statusBarBrightness:
+                  Brightness.light // Dark == white status bar -- for IOS.
+              ),
+          child: Center(
+              child: Text(
+            "Sembra che tu non abbia ancora seguito nessun itinerario.\nChe aspetti!!",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          )));
 
     return AnnotatedRegion<SystemUiOverlayStyle>(
-    value: SystemUiOverlayStyle.dark.copyWith(
-   statusBarColor: Colors.black, // Color for Android
-   statusBarBrightness: Brightness.light // Dark == white status bar -- for IOS.
-), child:
-    SafeArea(
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-          Column(children: <Widget>[
-            SizedBox(
-              height: Platform.isIOS ? 20 : 0,
+        value: SystemUiOverlayStyle.dark.copyWith(
+            statusBarColor: Colors.black, // Color for Android
+            statusBarBrightness:
+                Brightness.light // Dark == white status bar -- for IOS.
             ),
-            Platform.isAndroid
-                ? Text("")
-                : Padding(
-                    padding: EdgeInsets.only(left: 20, bottom: 10),
-                    child: Text("Seguiti",
-                        style: TextStyle(
-                            fontSize: 40, fontWeight: FontWeight.bold)))
-          ]),
-          Expanded(
-              child: Padding(
-                  padding: const EdgeInsets.only(
-                      left: 10, right: 10, bottom: 6, top: 0), // it was top: 8 for iOS
-                  child: ListView.builder(
-                    itemCount: userVisits.length,
-                    itemBuilder: (BuildContext ctxt, int index) {
-                      return VisitCard(
-                        visit: userVisits.elementAt(index),
-                      );
-                    },
-                    //),
-                    //),
-                  )))
-        ])));
+        child: SafeArea(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+              Column(children: <Widget>[
+                SizedBox(
+                  height: Platform.isIOS ? 20 : 0,
+                ),
+                Platform.isAndroid
+                    ? Text("")
+                    : Padding(
+                        padding: EdgeInsets.only(left: 20, bottom: 10),
+                        child: Text("Seguiti",
+                            style: TextStyle(
+                                fontSize: 40, fontWeight: FontWeight.bold)))
+              ]),
+              Expanded(
+                  child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 10,
+                          right: 10,
+                          bottom: 6,
+                          top: 0), // it was top: 8 for iOS
+                      child: ListView.builder(
+                        itemCount: userVisits.length,
+                        itemBuilder: (BuildContext ctxt, int index) {
+                          return VisitCard(
+                            visit: userVisits.elementAt(index),
+                          );
+                        },
+                      )))
+            ])));
   }
 }
